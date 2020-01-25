e429cfecf57382410c8af7943719b86fc0859ce3
Oleh-Lee <rrrgo3@gmail.com>
2020-01-26 00:34:56 +0200

    done

---
+++ task-6.js
@@ -0,0 +1,35 @@
+'use strict'
+const products = [{
+        name: 'Радар',
+        price: 1300,
+        quantity: 4
+    },
+    {
+        name: 'Сканер',
+        price: 2700,
+        quantity: 3
+    },
+    {
+        name: 'Дроид',
+        price: 400,
+        quantity: 7
+    },
+    {
+        name: 'Захват',
+        price: 1200,
+        quantity: 2
+    },
+];
+
+const calculateTotalPrice = function (products, productName) {
+    let total = 0;
+    for (const items of products) {
+        if (productName === items.name) {
+            total = items.price * items.quantity
+        }
+    }
+    return total;
+};
+
+console.log(calculateTotalPrice(products, 'Радар')); // 5200
+console.log(calculateTotalPrice(products, 'Дроид')); // 2800
